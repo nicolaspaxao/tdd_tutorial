c2d314905539a8ff3b00b171a2a197184e627b05
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-02-15 12:44:15 -0300

    first commit

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+  final int statusCode;
+
+  const Failure({required this.message, required this.statusCode});
+
+  @override
+  List<Object> get props => [message, statusCode];
+}
+
+class ApiFailure extends Failure {
+  const ApiFailure({required super.message, required super.statusCode});
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure({required super.message, required super.statusCode});
+}
---
+++ lib/core/utils/typedef.dart
@@ -0,0 +1,5 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd_tutorial/core/errors/failure.dart';
+
+typedef ResultFuture<Sucess> = Future<Either<Failure, Sucess>>;
+typedef ResultVoid = ResultFuture<void>;
